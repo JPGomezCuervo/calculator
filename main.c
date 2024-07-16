e39085113548746c695445351d39e92ee08d78af
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2024-07-15 19:17:14 -0500

    example of the basic usage

--- main.c
+++ main.c
@@ -4,16 +4,26 @@
 //TODO: Handle implicit multiplication eg. 2(2)
 int main(int argsc, char **argsv)
 {
+        (void) argsc;
         /* number of history elements */
         Calculator *calculator = init_calculator(5);
 
-        if (argsc != 2)
-        {
-                printf("USE: calc <expr>\n");
-        }
-
         double result = calculate_expr(calculator, argsv[1]);
-        printf("%.2f\n", result);
+        Expression **history = get_history(calculator);
+
+        if (!error_code(calculator))
+        {
+                printf("%.2f\n", result);
+                printf("HISTORY\n");
+                for (size_t i = 0; i < get_history_len(calculator); i++)
+                {
+                        printf("id: %d, expr: %s, res: %.2f\n",
+                                        history[i]->id,
+                                        history[i]->expr,
+                                        history[i]->result
+                                        );
+                }
+        }
 
         destroy_calculator(calculator);
 }
