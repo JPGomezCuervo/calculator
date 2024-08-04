a437a6ebcbd3c09b8d065eab9ce380ca33f64102
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2024-08-04 12:30:11 -0500

    first attempt integrating readline

--- Makefile
+++ Makefile
@@ -4,11 +4,12 @@ FLAGS = -Wall -Wextra -pedantic -ggdb -Wunused-function -Wmissing-prototypes -Wu
 OBJECTS = main.o calc.o
 CFILES = main.c calc.c
 BINARY = calc
+LIBS = -lreadline
 
 all: $(BINARY)
 
 $(BINARY): $(OBJECTS)
-	$(CC) $(FLAGS) -o $@ $^
+	$(CC) $(FLAGS) -o $@ $^ $(LIBS)
 
 %.o:%.c calc.h calc_internal.h
 	$(CC) $(FLAGS) -c -o $@ $<
--- main.c
+++ main.c
@@ -1,29 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "calc.h"
 
 /* THIS IS AN EXAMPLE ON HOW TO USE THE LIBRARY */
 int main(int argsc, char **argsv)
 {
-        (void) argsc;
         /* number of history elements */
         Calculator *calculator = init_calculator(5);
 
-        double result = calculate_expr(calculator, argsv[1]);
-        Expression **history = get_history(calculator);
-
-        if (!get_error_code(calculator))
+        if (argsc > 1)
         {
-                printf("%.2f\n", result);
-                printf("HISTORY\n");
-                for (size_t i = 0; i < get_history_len(calculator); i++)
+                double result = calculate_expr(calculator, argsv[1]);
+                Expression **history = get_history(calculator);
+
+                if (!get_error_code(calculator))
                 {
-                        printf("id: %d, expr: %s, res: %.2f\n",
-                                        history[i]->id,
-                                        history[i]->expr,
-                                        history[i]->result
-                                        );
+                        printf("%.2f\n", result);
+                        printf("HISTORY\n");
+                        for (size_t i = 0; i < get_history_len(calculator); i++)
+                        {
+                                printf("id: %d, expr: %s, res: %.2f\n",
+                                                history[i]->id,
+                                                history[i]->expr,
+                                                history[i]->result);
+                        }
+                }
+        }
+        else
+        {
+                char *line;
+                        printf("Entering interactive mode...\n");
+                while ((line = readline(">> ")) != NULL)
+                {
+                        if (*line)
+                        {
+                                add_history(line);
+                                double result = calculate_expr(calculator, line);
+
+                                if (!get_error_code(calculator))
+                                        printf("%.2f\n", result);
+                        }
+                        free(line);
+                }
+                if (line == NULL)
+                {
+                        free(line);
+                        printf("\nExiting interactive mode...\n");
                 }
         }
 
         destroy_calculator(calculator);
+        return 0;
 }
